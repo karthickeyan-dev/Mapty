cc1e7d2418eec0fdd9cf2cd8e0e97a6f05f479e7
karthickeyan <jkarthickeyan24@gmail.com>
2024-12-11 17:56:54 +0530

    render workout on map

--- script.js
+++ script.js
@@ -10,3 +10,149 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // km
+    this.duration = duration; // min
+  }
+}
+
+class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+class App {
+  #map;
+  #mapEvent;
+  #workout = [];
+
+  constructor() {
+    this.#getPosition();
+    inputType.addEventListener('change', this.#toggleInputField);
+    form.addEventListener('submit', this.#newWorkout.bind(this));
+  }
+
+  #getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.#loadMap.bind(this),
+        function () {
+          alert('Please allow location to use this website');
+        }
+      );
+  }
+
+  #loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+    this.#map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(
+      this.#map
+    );
+    // Call showForm on map click
+    this.#map.on('click', this.#showForm.bind(this));
+  }
+
+  #showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+    // console.log(this.#mapEvent);
+  }
+
+  #toggleInputField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  #newWorkout(e) {
+    e.preventDefault();
+    const coords = [this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng];
+    const type = inputType.value;
+    const distance = +inputDistance.value; // convert to number
+    const duration = +inputDuration.value; // convert to number
+    let workout;
+    // Input Validation
+    const inputValid = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+    const inputPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        !inputValid(distance, duration, cadence) ||
+        !inputPositive(distance, duration, cadence)
+      )
+        return alert('Invalid inputs');
+      // Create new running object
+      workout = new Running(coords, distance, duration, cadence);
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !inputValid(distance, duration, elevation) ||
+        !inputPositive(distance, duration)
+      )
+        return alert('Invalid inputs');
+      // Create new cycling object
+      workout = new Cycling(coords, distance, duration, elevation);
+    }
+    // Add new objects to #workout array
+    this.#workout.push(workout);
+    //Add market to map
+    this.#renderWorkoutMarker(workout);
+    //Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    form.classList.add('hidden');
+  }
+
+  #renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        `${workout.type[0].toUpperCase() + workout.type.slice(1)} on ${
+          months[workout.date.getMonth()]
+        } ${workout.date.getDate()}`,
+        {
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        }
+      )
+      .openPopup();
+  }
+}
+
+const app = new App();
